5147043f3098717a0dedc1ae902be6bb4813b86e
ZHANG, Meng <stall-breezes.0t@icloud.com>
2024-10-31 16:46:03 +1300

    Add Annotation Models

---
+++ models/annot/l1_annot_pred_cls.sql
@@ -0,0 +1,61 @@
+{{ config(materialized='table') }}
+
+SELECT 1                                          AS category
+     , 1                                          AS labeled
+     , REGEXP_EXTRACT(image, '_h\d+_(\d+)_', 1)   AS prefix
+     , REGEXP_EXTRACT(image, '_\d+_(\w+\..+)', 1) AS image
+     , REGEXP_EXTRACT(image, '_y(\d+)_', 1)       AS y
+     , REGEXP_EXTRACT(image, '_x(\d+)_', 1)       AS x
+     , REGEXP_EXTRACT(image, '_h(\d+)_', 1)       AS h
+     , REGEXP_EXTRACT(image, '_w(\d+)_', 1)       AS w
+     , class
+     , label_top1
+     , score_top1
+     , label_top2
+     , score_top2
+  FROM {{ ref('l0_annot_pred_labeled_cls_category') }}
+ UNION ALL
+SELECT 0                                          AS category
+     , 1                                          AS labeled
+     , REGEXP_EXTRACT(image, '_h\d+_(\d+)_', 1)   AS prefix
+     , REGEXP_EXTRACT(image, '_\d+_(\w+\..+)', 1) AS image
+     , REGEXP_EXTRACT(image, '_y(\d+)_', 1)       AS y
+     , REGEXP_EXTRACT(image, '_x(\d+)_', 1)       AS x
+     , REGEXP_EXTRACT(image, '_h(\d+)_', 1)       AS h
+     , REGEXP_EXTRACT(image, '_w(\d+)_', 1)       AS w
+     , class
+     , label_top1
+     , score_top1
+     , label_top2
+     , score_top2
+  FROM {{ ref('l0_annot_pred_labeled_cls_severity') }}
+ UNION ALL
+SELECT 1                                          AS category
+     , 0                                          AS labeled
+     , REGEXP_EXTRACT(image, '_h\d+_(\d+)_', 1)   AS prefix
+     , REGEXP_EXTRACT(image, '_\d+_(\w+\..+)', 1) AS image
+     , REGEXP_EXTRACT(image, '_y(\d+)_', 1)       AS y
+     , REGEXP_EXTRACT(image, '_x(\d+)_', 1)       AS x
+     , REGEXP_EXTRACT(image, '_h(\d+)_', 1)       AS h
+     , REGEXP_EXTRACT(image, '_w(\d+)_', 1)       AS w
+     , class
+     , label_top1
+     , score_top1
+     , label_top2
+     , score_top2
+  FROM {{ ref('l0_annot_pred_unlabeled_cls_category') }}
+ UNION ALL
+SELECT 0                                          AS category
+     , 0                                          AS labeled
+     , REGEXP_EXTRACT(image, '_h\d+_(\d+)_', 1)   AS prefix
+     , REGEXP_EXTRACT(image, '_\d+_(\w+\..+)', 1) AS image
+     , REGEXP_EXTRACT(image, '_y(\d+)_', 1)       AS y
+     , REGEXP_EXTRACT(image, '_x(\d+)_', 1)       AS x
+     , REGEXP_EXTRACT(image, '_h(\d+)_', 1)       AS h
+     , REGEXP_EXTRACT(image, '_w(\d+)_', 1)       AS w
+     , class
+     , label_top1
+     , score_top1
+     , label_top2
+     , score_top2
+  FROM {{ ref('l0_annot_pred_unlabeled_cls_severity') }}
